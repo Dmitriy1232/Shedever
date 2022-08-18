e95837abfd330eb5ea176347465cc9a57d5d80d3
Dmitriy1232 <37824553+Dmitriy1232@users.noreply.github.com>
2022-08-18 21:49:59 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.slider__body').slick({
+		slidesToShow: 4,
+		adaptiveHeight:false,
+		autoplay:false,
+		speed:500,
+		arrows:true,
+		waitForAnimate:false,
+		cssEase:'ease',
+		easing:'ease',
+		variableWidth: true,
+	});
+});//
